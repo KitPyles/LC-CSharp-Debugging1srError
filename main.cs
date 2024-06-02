9c869d88b3f357c3c723c6a629a9838efba2d91b
Kit Pyles <117668261+KitPyles@users.noreply.github.com>
2024-06-02 02:11:43 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,22 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+    //Run this code first and examine the error message.
+    //Fix the syntax error then run the code again to check your work.
+
+    bool launchReady = false;
+    int fuelLevel = 17000;
+
+    if (fuelLevel >= 20000 
+    { 
+      Console.WriteLine("Fuel level cleared.");
+      Console.WriteLine(!launchReady);
+    } 
+    else 
+    {
+      Console.WriteLine("WARNING: Insufficient fuel!");
+      Console.WriteLine(launchReady);
+    }
+  }
+}
